e72ac4c50eb1a96307ff99b77e1c8c9b570a16d6
Mathieu Gagne <gagne.mathieu@hotmail.com>
2015-10-29 15:56:46 -0400

    Additional answers

--- 08-concurrency_channels/03-channels/exercises/template1/template1.go
+++ 08-concurrency_channels/03-channels/exercises/template1/template1.go
@@ -1,7 +1,5 @@
 // All material is licensed under the Apache License Version 2.0, January 2004
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// http://play.golang.org/p/G7O-DnJrEA
+// http://www.apache.org/licenses/LICENSE-2.0// http://play.golang.org/p/G7O-DnJrEA
 
 // Write a program where two goroutines pass an integer back and forth
 // ten times. Display when each goroutine receives the integer. Increment
@@ -10,32 +8,48 @@
 package main
 
 // Add imports.
+import (
+	"fmt"
+	"sync"
+)
 
 // Declare a wait group variable.
+var wg sync.WaitGroup
 
 // Declare a function for the goroutine. Pass in a name for the
 // routine and a channel of type int used to share the value back and forth.
-func goroutine( /* parameters */ ) {
+func goroutine(name string, channel chan int) {
 	for {
 		// Receive on the channel, waiting for the integer.
+		number, ok := <-channel
 
 		// Check if the channel is closed.
-		{
+		if !ok {
 			// Call done on the waitgroup.
+			wg.Done()
 			// Display the goroutine is finished and return.
+			fmt.Println("goroutine over")
+			return
 		}
 
 		// Display the integer value received.
+		fmt.Println("Received: ", number)
 
 		// Check is the value from the channel is 10.
-		{
+		if number == 10 {
 			// Close the channel.
+			close(channel)
 			// Call done on the waitgroup.
+			wg.Done()
 			// Display the goroutine is finished and return.
+			fmt.Println("goroutine over, reached 10")
+			return
 		}
 
 		// Increment the value by one and send in back through
 		// the channel.
+		number++
+		channel <- number
 	}
 }
 
@@ -43,13 +57,19 @@ func goroutine( /* parameters */ ) {
 func main() {
 	// Declare and initialize an unbuffered channel
 	// of type int.
+	channel := make(chan int)
 
 	// Increment the wait group for each goroutine
 	// to be created.
+	wg.Add(2)
 
 	// Create the two goroutines.
+	go goroutine("first routine", channel)
+	go goroutine("second routine", channel)
 
 	// Send the initial integer value into the channel.
+	channel <- 1
 
 	// Wait for all the goroutines to finish.
+	wg.Wait()
 }
--- 08-concurrency_channels/03-channels/exercises/template2/template2.go
+++ 08-concurrency_channels/03-channels/exercises/template2/template2.go
@@ -12,37 +12,58 @@
 package main
 
 // Add Imports.
+import (
+	"fmt"
+	"sync"
+)
 
 // Declare a constant and set the value for the number of goroutines.
+const numGoRoutines = 20
 
 // Declare a constant and set the value for the number of buffers.
+const numBuffers = 4
 
 // Declare a wait group.
+var wg sync.WaitGroup
 
 // Declare a buffered channel of type string and initialize it based on
 // the constant you declared above.
+var channel = make(chan string, numBuffers)
 
 // Declare a function for the goroutine that will process work
 // from the buffered channel.
 func worker(worker int) {
 	// Receive a string from the channel.
+	value := <-channel
 
 	// Display the string.
+	fmt.Println(worker, value)
 
 	// Send the string back into the channel.
+	channel <- value
 
 	// Tell main this goroutine is done.
+	wg.Done()
 }
 
 // main is the entry point for all Go programs.
 func main() {
 	// Increment the wait group for the number of
 	// goroutines based on the value of the constant.
+	wg.Add(numGoRoutines)
 
 	// Create the number of goroutines based on the
 	// value of the constant.
+	for i := 0; i < numGoRoutines; i++ {
+		go worker(i)
+	}
 
 	// Add strings in the buffered channel.
+	channel <- "A"
+	channel <- "B"
+	channel <- "C"
+	channel <- "D"
 
 	// Wait for all the work to get done.
+	wg.Wait()
 }
